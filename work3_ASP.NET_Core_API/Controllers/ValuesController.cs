e542f2db0ba0a6b7e8b5e2ce2d3d8acb9371414b
DmchFast <chdima28@gmail.com>
2026-04-08 01:51:52 +0300

    Initializing commit

---
+++ work3_ASP.NET_Core_API/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace work3_ASP.NET_Core_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
